3470d2649b4fd6fdd6e9eb7534a503d8c7a733d8
waezahmed009 <waezahmedrumman99@gmail.com>
2026-05-11 18:06:18 +0600

    Update script.js

--- script.js
+++ script.js
@@ -13,427 +13,427 @@ window.addEventListener('mousemove', (e) => {
 const rollmates = {
 
     "2513001": {
-        namereal:"kafi",
+        namereal:"SYED AIMAN HOQ",
         rollmate: "2413001",
         name: "Shihab Hossain",   
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513002": {
-        namereal:"kafi",
+        namereal:"ADNAN SAMI",
         name: "Tanvir Ahmed",
         rollmate: "2413002",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513003": {
-        namereal:"kafi",
+        namereal:"PROTASHA BISWAS",
         name: "Mehedi Hassan",
         rollmate: "2413003",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513004": {
-        namereal:"kafi",
+        namereal:"MD. TAMJID SIAM MAHIR",
         name: "Rakibul Islam",
         rollmate: "2413004",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513005": {
-        namereal:"kafi",
+        namereal:"MAHIN ZAMAN AHON",
         name: "Sabbir Rahman",
         rollmate: "2413005",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513006": {
-        namereal:"kafi",
+        namereal:"MARIA RUBYAT",
         name: "Nusrat Jahan",
         rollmate: "2413006",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513007": {
-        namereal:"kafi",
+        namereal:"MD. MIRAJ ALI",
         name: "Fariha Tasnim",
         rollmate: "2413007",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513008": {
-        namereal:"kafi",
+        namereal:"MD MUNTASIR HASAN SOHAN",
         name: "Imran Hossain",
         rollmate: "2413008",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513009": {
-        namereal:"kafi",
+        namereal:"MD. MAHIR LABIB",
         name: "Sourov Dey",
         rollmate: "2413009",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513010": {
-        namereal:"kafi",
+        namereal:"NILOY CHOWDHURY",
         name: "Tahmina Akter",
         rollmate: "2413010",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513011": {
-        namereal:"kafi",
+        namereal:"SHADMAN SHADAB SHIRSHO",
         name: "Arif Billah",
         rollmate: "2413011",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513012": {
-        namereal:"kafi",
+        namereal:"MISHKAT NOOR",
         name: "Nazmul Haque",
         rollmate: "2413012",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513013": {
-        namereal:"kafi",
+        namereal:"MD SALMAN SABIT",
         name: "Sumaiya Khanam",
         rollmate: "2413013",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513014": {
-        namereal:"kafi",
+        namereal:"SANJIDA JANNAT PRITY",
         name: "Jubayer Ahmed",
         rollmate: "2413014",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513015": {
-        namereal:"kafi",
+        namereal:"MAHBUBA ISLAM",
         name: "Lamia Sultana",
         rollmate: "2413015",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513016": {
-        namereal:"kafi",
+        namereal:"S.M. TAHMIDUR RAHMAN",
         name: "Rifat Hasan",
         rollmate: "2413016",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513017": {
-        namereal:"kafi",
+        namereal:"RAHUL SAHA",
         name: "Maisha Maliha",
         rollmate: "2413017",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513018": {
-        namereal:"kafi",
+        namereal:"SOURAV KUMAR JOY",
         name: "Raihanul Kabir",
         rollmate: "2413018",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513019": {
-        namereal:"kafi",
+        namereal:"ISHTIAK HOSSAIN NOMAN",
         name: "Shanta Islam",
         rollmate: "2413019",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513020": {
-        namereal:"kafi",
+        namereal:"SHIHAB",
         name: "Touhidul Alam",
         rollmate: "2413020",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513021": {
-        namereal:"kafi",
+        namereal:"AHNAF ZARIF",
         name: "Nadia Afrin",
         rollmate: "2413021",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513022": {
-        namereal:"kafi",
+        namereal:"MD. ASRAFUL ISLAM NIROB",
         name: "Shahriar Kabir",
         rollmate: "2413022",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513023": {
-        namereal:"kafi",
+        namereal:"MAHIA TABASSUM BORSHA",
         name: "Israt Jahan",
         rollmate: "2413023",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513024": {
-        namereal:"kafi",
+        namereal:"MD. WASIR SHIKDER RATUL",
         name: "Minhajul Islam",
         rollmate: "2413024",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513025": {
-        namereal:"kafi",
+        namereal:"ARIFUL ISLAM",
         name: "Tanjila Akter",
         rollmate: "2413025",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513026": {
-        namereal:"kafi",
+        namereal:"SHEIKH MOHAMMAD MAHADI HASAN",
         name: "Asif Iqbal",
         rollmate: "2413026",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513027": {
-        namereal:"kafi",
+        namereal:"MD. SALAH UDDIN MIKDAD",
         name: "Priya Saha",
         rollmate: "2413027",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513028": {
-        namereal:"kafi",
+        namereal:"SK. MOFSIN TAMIM",
         name: "Mushfiqur Rahman",
         rollmate: "2413028",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513029": {
-        namereal:"kafi",
+        namereal:"ABU JAR GIFARI AKAND",
         name: "Bristy Roy",
         rollmate: "2413029",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513030": {
-        namereal:"kafi",
+        namereal:"MUTTAKIN FUAD",
         name: "Zahirul Hasan",
         rollmate: "2413030",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513031": {
-        namereal:"kafi",
+        namereal:"JAMINUR RAHMAN",
         name: "Sadia Sultana",
         rollmate: "2413031",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513032": {
-        namereal:"kafi",
+        namereal:"MD. RIAD ISLAM",
         name: "Kawsar Hamid",
         rollmate: "2413032",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513033": {
-        namereal:"kafi",
+        namereal:"PRATTOY BANIK",
         name: "Tamanna Yesmin",
         rollmate: "2413033",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513034": {
-        namereal:"kafi",
+        namereal:"JUNAYED BUGDADI DIPTA",
         name: "Bipul Chandra",
         rollmate: "2413034",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513035": {
-        namereal:"kafi",
+        namereal:"MD. ISHAK RAHMAN",
         name: "Sharmin Akter",
         rollmate: "2413035",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513036": {
-        namereal:"kafi",
+        namereal:"MD. NIAMUL HASAN",
         name: "Morshed Alam",
         rollmate: "2413036",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513037": {
-        namereal:"kafi",
+        namereal:"SHOUVIK BASAK SHOSHI",
         name: "Roksana Begum",
         rollmate: "2413037",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513038": {
-        namereal:"kafi",
+        namereal:"MEHEDI AL HASAN SIAM",
         name: "Golam Rabbani",
         rollmate: "2413038",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513039": {
-        namereal:"kafi",
+        namereal:"MD. SAIFUR RAHMAN SIAM",
         name: "Monira Khatun",
         rollmate: "2413039",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513040": {
-        namereal:"kafi",
+        namereal:"MD IMAM HASAN JEWEL",
         name: "Rezaul Karim",
         rollmate: "2413040",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513041": {
-        namereal:"kafi",
+        namereal:"SAWCHA PODDER",
         name: "Fahmida Nasrin",
         rollmate: "2413041",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513042": {
-        namereal:"kafi",
+        namereal:"ROFAT ROYAN",
         name: "Towfiq Hasan",
         rollmate: "2413042",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513043": {
-        namereal:"kafi",
+        namereal:"MAISUN MEHAJABIN",
         name: "Sufia Khanam",
         rollmate: "2413043",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513044": {
-        namereal:"kafi",
+        namereal:"SIYAM AHMED",
         name: "Mosaddek Hossain",
         rollmate: "2413044",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513045": {
-        namereal:"kafi",
+        namereal:"MD. MUBTASIM SAROWAR RIHAN",
         name: "Afsana Mimi",
         rollmate: "2413045",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513046": {
-        namereal:"kafi",
+        namereal:"RIGWAN MUSTAFA",
         name: "Saiful Islam",
         rollmate: "2413046",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513047": {
-        namereal:"kafi",
+        namereal:"AFNAN AMJAD",
         name: "Rabeya Khatun",
         rollmate: "2413047",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513048": {
-        namereal:"kafi",
+        namereal:"MD. ASHIQUR RAHMAN MOZUMDER",
         name: "Kamruzzaman Siddique",
         rollmate: "2413048",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513049": {
-        namereal:"kafi",
+        namereal:"ZUANA TASHRIN",
         name: "Halima Akter",
         rollmate: "2413049",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513050": {
-        namereal:"kafi",
+        namereal:"SAJID BIN MAIN",
         name: "Mahmudul Hasan",
         rollmate: "2413050",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513051": {
-        namereal:"kafi",
+        namereal:"MD. RIFAT NABIL SIDDIQUE",
         name: "Parvin Akter",
         rollmate: "2413051",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513052": {
-        namereal:"kafi",
+        namereal:"SABBIR AHMED",
         name: "Shafiul Azam",
         rollmate: "2413052",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513053": {
-        namereal:"kafi",
+        namereal:"KAYES MAHMUD SHAKKHOR",
         name: "Mou Akter",
         rollmate: "2413053",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513054": {
-        namereal:"kafi",
+        namereal:"MD. RAQUIB AHSAN TAMZID",
         name: "Hasibul Hasan",
         rollmate: "2413054",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513055": {
-        namereal:"kafi",
+        namereal:"MD. TANIM HOSSEN",
         name: "Dilruba Yesmin",
         rollmate: "2413055",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513056": {
-        namereal:"kafi",
+        namereal:"UTSHA BHOWMIK",
         name: "Anisur Rahman",
         rollmate: "2413056",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513057": {
-        namereal:"kafi",
+        namereal:"MD. HASIBUL HASAN",
         name: "Waez Ahmed Rumman",
         rollmate: "2413057",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513058": {
-        namereal:"kafi",
+        namereal:"MST. MODINA MONAWARA",
         name: "Jannatul Ferdaus",
         rollmate: "2413058",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513059": {
-        namereal:"kafi",
+        namereal:"SOHAN ALAHI SUNNY",
         name: "Obaidullah Al Maruf",
         rollmate: "2413059",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513060": {
-        namereal:"kafi",
+        namereal:"MD. HANIF",
         name: "Fatema Tuz Zohura",
         rollmate: "2413060",
         whatsapp: "YOUR_WHATSAPP_LINK",
         facebook: "YOUR_FACEBOOK_LINK"
     },
     "2513061": {
-        namereal:"kafi",
+        namereal:"SABAHAT MUSTARIN",
         name: "Admin",
         rollmate: "ALL ACCESS",
         whatsapp: "YOUR_WHATSAPP_LINK",
