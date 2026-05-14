ef3eda74b83a61287ec678678966951ddebeed61
waezahmed009 <waezahmedrumman99@gmail.com>
2026-05-14 18:30:53 +0600

    Update script.js

--- script.js
+++ script.js
@@ -189,7 +189,7 @@ const rollmates = {
     },
     "2513026": {
         namereal:"SHEIKH MOHAMMAD MAHADI HASAN",
-        name: "RAFID BIN AKHTER",
+        name: "not found",
         rollmate: "2413026",
         whatsapp: "https://wa.me/8801835468579",
         facebook: "YOUR_FACEBOOK_LINK"
